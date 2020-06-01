a5ce1c4ad0586846e53c362f49d9c312c33a9df6
fearless-command-2358 <iuriseara@gmail.com>
2020-06-01 19:03:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,7 +141,7 @@ def adding_to_dennis
       }
     }
 
-		programmer_hash[:dennis_ritchie] = [:Assembly]
+		programmer_hash[:dennis_ritchie][:languages] << ["C", "Assembly"]
 		return programmer_hash
 
 end
