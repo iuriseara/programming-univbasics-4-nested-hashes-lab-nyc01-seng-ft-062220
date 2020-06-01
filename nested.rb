914e482e7034973b38d01031d8698746d63f67d1
fearless-command-2358 <iuriseara@gmail.com>
2020-06-01 18:43:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,11 +88,11 @@ def adding_matz
       }
     }
 
-	
+		#programmer_hash[:yukihiro_matsumoto] = {}
 		programmer_hash[:yukihiro_matsumoto] << {known_for: "Ruby"}
-		programmer_hash[:yukihiro_matsumoto] << {languages: "LISP"}
+		programmer_hash[:yukihiro_matsumoto] << {languages: ["LISP", "C"]}
 
-		programmer_hash
+		return programmer_hash
 
 end
 
